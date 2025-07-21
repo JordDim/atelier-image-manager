aadf1321c786a5ffa7b4f342bdb2e403cc99c233
JordDim <dimelowjordan@gmail.com>
2025-07-21 20:28:52 +0200

    Added docker files and unversioned files

---
+++ src/main/java/com/freelance/atelier_image_manager/AtelierImageManagerApplication.java
@@ -0,0 +1,13 @@
+package com.freelance.atelier_image_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierImageManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelierImageManagerApplication.class, args);
+	}
+
+}
--- src/main/java/com/freelance/atelier_image_manager/model/Image.java
+++ src/main/java/com/freelance/atelier_image_manager/model/Image.java
@@ -12,12 +12,12 @@ import lombok.*;
 @Table(name = "image")
 public class Image {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "image_seq")
-    @SequenceGenerator(name = "image_seq", sequenceName = "image_seq", allocationSize = 1)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "image_seq")
+	@SequenceGenerator(name = "image_seq", sequenceName = "image_seq", allocationSize = 1)
+	private Long id;
 
-    @Column(name = "name")
-    private String name;
+	@Column(name = "name")
+	private String name;
 
 }
